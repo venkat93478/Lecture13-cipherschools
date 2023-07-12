bda01b2abe2aae6145c2d4977f3a01716f2bd7a2
Venkata sai <116934116+venkat93478@users.noreply.github.com>
2023-07-12 09:44:05 +0530

    Create C.java

---
+++ C.java
@@ -0,0 +1,17 @@
+package accessmodifiers2;
+import accessmodifiers.A;
+
+
+public class C extends A {
+	public static void main(String [] args) {
+		A obj = new A();
+		C childobj = new C();
+		//System.out.println(obj.x);
+		//System.out.println(childobj.y);//not accessible outside the class
+		System.out.println(obj.z);
+		System.out.println(childobj.w);
+		
+	}
+	
+
+}
